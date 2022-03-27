dea048538f371923c2b37fa55ef7b78ca756c095
murlaus <murlaus44@gmail.com>
2022-03-27 23:47:33 +0300

    first commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,16 +7,52 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-
+<style>
+    h1{
+      text-align: center;
+        justify-content: flex-start;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    form{
+        text-align: center;
+        justify-content: flex-start;
+        font: 1em sans-serif;
+    }
+    input{
+ text-align: center;
+        justify-content: flex-start;
+        font: 1em sans-serif;
+        border-top-left-radius: 2px;
+    }
+    p{
+        font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        color:gray;
+    }
+</style>
 <body>
-
-    <form action="/about" method="post">
+   <h1>Wellcome</h1>
+    <form action="/about" method="post" >
         @csrf
         <input type="text" name="name" id="">
+        <br>
+        <br>
         <input type="submit" value="send">
+        <br>
+        <br>
+
+
+    @if (isset($name))
+        <p>Name</p>
+        <input type="text" name="" value="{{$name}}" id="">
+
+
+    @endif
+
     </form>
-    
-    
+
+
+
+
 </body>
 
 </html>
