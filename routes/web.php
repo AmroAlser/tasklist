dd1e2ec3f02724c29001b45123e6b935bece50cb
murlaus <murlaus44@gmail.com>
2022-03-27 23:19:19 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/tasks', function () {
+    $tasks = ['task1', 'task2', 'task3'];
+
+    return view('tasks', compact('tasks'));
+});
+Route::get('show/{id}', function ($id) {
+    $tasks = ['task1', 'task2', 'task3'];
+    $task = $tasks[$id];
+    return view('show', compact('task'));
+});
+Route::get('show', function () {
+    $tasks = ['task1', 'task2', 'task3'];
+
+
+    return view('show');
+});
+Route::post('/about', function () {
+
+    $name = $_POST['name'];
+    return view('about', compact('name'));
+});
+Route::get('/about', function () {
+
+
+    return view('about');
+});
