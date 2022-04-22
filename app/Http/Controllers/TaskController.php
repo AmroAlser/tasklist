70eb69ac8c4644bfe2297ef58c8f5ad3c95240b7
murlaus <murlaus44@gmail.com>
2022-04-23 02:44:10 +0300

    Eloquent homework

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
 class TaskController extends Controller
 {
     //
-    public function index(){
-      $tasks = DB::table('tasks')->orderBy('name')->get();
+    public function index()
+    {
+        // $tasks = DB::table('tasks')->orderBy('name')->get();
 
-      //  $tasks = Task::all();
+        $tasks = Task::all()->sortBy('name');
         return view('tasks.index', compact('tasks'));
     }
     public function show($id)
@@ -20,46 +22,49 @@ class TaskController extends Controller
 
         return view('show', compact('task'));
     }
-    public function store(Request $request){
-       $task = DB::table('tasks')->insert([
-            'name'=>$request->name,
-            'created_at' => now(),
-            'updated_at'=>now()
-      ]);
-    //   $validated=$request->validate([
-    //       'name'=>'required|max:15 | min:3|string'
-    //   ]);
-    // $task = new Task ();
-    // $task ->name = $request->name;
-    // $task->save();
+    public function store(Request $request)
+    {
+        // $task = DB::table('tasks')->insert([
+        //     'name' => $request->name,
+        //     'created_at' => now(),
+        //     'updated_at' => now()
+        // ]);
+        $validated = $request->validate([
+            'name' => 'required|max:15 | min:3|string'
+        ]);
+        $task = new Task();
+        $task->name = $request->name;
+        $task->save();
         return redirect()->back();
     }
-    public function delete ($id){
-       DB::table('tasks')->where('id','=',$id)->delete();
-       //Task::find($id)->delete();
+    public function delete($id)
+    {
+        // DB::table('tasks')->where('id', '=', $id)->delete();
+        Task::find($id)->delete();
         return redirect()->back();
     }
     public function edit($id)
     {
-       $tasks= DB::table('tasks')->orderBy('name')->get();
-         $task=DB::table('tasks')->find($id);
-        //$task = Task::find($id);
-
-        return view('tasks.edit', compact('task','tasks'));
-
+        //$tasks = DB::table('tasks')->orderBy('name')->get();
+        //  $task = DB::table('tasks')->find($id);
+        $task = Task::find($id);
+        $tasks = Task::all()->sortBy('name');
+        return view('tasks.edit', compact('task', 'tasks'));
     }
 
     public function update(Request $request, $id)
     {
-       // $task = Task::find($id);
 
-      $task= DB::table('tasks')
-            ->where('id', '=',$id)
-            ->update(['name' => $request->name]);
+        // $tasks = DB::table('tasks')->orderBy('name')->get();
 
-       $tasks=DB::table('tasks')->orderBy('name')->get();
 
+        // $task = DB::table('tasks')
+        //     ->where('id', '=', $id)
+        //     ->update(['name' => $request->name]);
+        $tasks = Task::all()->sortBy('name');
+        $task = Task::find($id);
+        $task->name = $request->name;
+        $task->save();
         return redirect('/');
-
     }
 }
